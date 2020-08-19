33987c99bffdabcafee33630b34a2748f9675c28
ocya <38377412+ocya@users.noreply.github.com>
2020-08-19 17:47:57 +0800

    Update 1080.cpp

--- 1080.cpp
+++ 1080.cpp
@@ -72,3 +72,66 @@ bool Compare( Data A, Data B ){
         return ( A.Total > B.Total );
     }
 }
+
+/*
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+typedef struct Node Data;
+struct Node{
+    std::string Name;
+    int PGrade, MGrade, NGrade, Total;
+};
+bool Compare( Data A, Data B ){
+    return A.Total != B.Total ? A.Total > B.Total : A.Name < B.Name;
+}
+int main( int argc, const char *argv[] ){
+
+    int P, M, N, Count, Score;
+    std::string S;
+    std::map<std::string,int> Idx;
+    std::vector<Data> GradeInfo;
+
+    std::cin >> P >> M >> N;
+    Count = 0;
+    for( int i = 0 ; i < P ; i++ ){
+        std::cin >> S >> Score;
+        if( Score >= 200 ){
+           GradeInfo.push_back( Node{ S, Score, -1, -1, 0 } );
+           Idx[S] = Count++;
+        }
+    }
+    for( int i = 0 ; i < M ; i++ ){
+        std::cin >> S >> Score;
+        if( Idx.find( S ) != Idx.end() ) GradeInfo[ Idx[S] ].MGrade = Score;
+    }
+    for( int i = 0 ; i < N ; i++ ){
+        std::cin >> S >> Score;
+        if( Idx.find( S ) != Idx.end() ){
+            GradeInfo[ Idx[S] ].NGrade = Score;
+        } 
+    }
+    std::vector<Data> Ans;
+    int Gmid, Gfinal;
+    for( auto i : GradeInfo ){
+        Gmid = i.MGrade;
+        Gfinal = i.NGrade;
+        i.Total = ( Gmid > Gfinal ) ? ( int )( Gmid * 0.4 + Gfinal * 0.6 + 0.5 ) : Gfinal ;
+        if( i.Total >= 60 ) Ans.push_back( i );        
+    }
+    sort( Ans.begin(), Ans.end(), Compare );
+    for( int i = 0 ; i < Ans.size() ; i++ ){
+        std::cout << Ans[i].Name << ' ' << Ans[i].PGrade << ' ' \
+                    << Ans[i].MGrade << ' ' << Ans[i].NGrade << ' '\
+                     << Ans[i].Total << std::endl;
+    }
+    
+    return 0;
+}
+
+
+
+*/
